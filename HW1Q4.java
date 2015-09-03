4ee1bd3fc6698ad1be55ffd489bf558ad132f78c
Luna777 <xinguan777@gmail.com>
2015-09-02 23:25:11 -0400

    Create HW1Q4.java

---
+++ HW1Q4.java
@@ -0,0 +1,20 @@
+// CISC 3150
+// Xin Guan
+// 9/2/2015
+
+import java.util.*;
+
+public class HW1Q4{
+	
+	public static void main(String[] args){
+		
+		Scanner my_sc = new Scanner(System.in);
+		
+		while(my_sc.hasNext()){
+			
+			System.out.println(my_sc.nextInt());
+		}
+		
+	}
+	
+}
